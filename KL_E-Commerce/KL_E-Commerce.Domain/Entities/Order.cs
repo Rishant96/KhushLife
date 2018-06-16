1237bbdb291662a04bbaf47110ac6ede7275c639
Rishant Dutt <kirarishant@outlook.com>
2018-06-16 20:45:33 +0530

    Models: superficial setup

--- KL_E-Commerce/KL_E-Commerce.Domain/Entities/Order.cs
+++ KL_E-Commerce/KL_E-Commerce.Domain/Entities/Order.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KL_E_Commerce.Domain.Abstract;
+using KL_E_Commerce.Domain.Entities.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,13 @@ namespace KL_E_Commerce.Domain.Entities
     public class Order
     {
         public int Id { get; set; }
-        // More Logic
+        public DateTime OrderDate { get; set; }
+        public float OrderValue { get; set; }
+        public IDeliveryProvider DeliveryMethod { get; set; }
+        public DateTime DeliverBy { get; set; }
+        public int UserId { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public virtual User User { get; set; }
     }
 }
--- KL_E-Commerce/KL_E-Commerce.Domain/Entities/Vendor.cs
+++ KL_E-Commerce/KL_E-Commerce.Domain/Entities/Vendor.cs
@@ -7,13 +7,8 @@ using System.Threading.Tasks;
 
 namespace KL_E_Commerce.Domain.Entities
 {
-    public class Vendor
+    public class Vendor : User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public Address Address { get; set; }
-        public string Password { get; set; }
     }
 }
